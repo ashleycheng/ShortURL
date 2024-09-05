1ff8f400761554f01e8314ecb6cece66802a75dc
ashleycheng <ashley830204@gmail.com>
2024-09-05 16:01:33 +0800

    feat: create a model URLMapping for storing original url and short url mapping info

--- shorturl/admin.py
+++ shorturl/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import URLMapping
 
-# Register your models here.
+
+class URLMappingAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        "short_url",
+        "expiration_date",
+    )
+    list_display = (
+        "short_url",
+        "expiration_date",
+    )
+
+
+admin.site.register(URLMapping, URLMappingAdmin)
--- shorturl/models.py
+++ shorturl/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import timedelta
+from django.utils.timezone import now
 
-# Create your models here.
+from sqids import Sqids
+
+
+def expiration(days: int) -> timedelta:
+    return now() + timedelta(days=days)
+
+
+class URLMapping(models.Model):
+    original_url = models.CharField(max_length=2048, blank=False)
+    short_url = models.CharField(max_length=20, unique=True)
+    expiration_date = models.DateTimeField()
+
+    @property
+    def get_unique_string(self):
+        if self.pk is None and URLMapping.objects.last():
+            new_pk = URLMapping.objects.last().id + 1
+        elif self.pk is None:
+            new_pk = 1
+        sqids = Sqids(min_length=8)
+        unique_string = sqids.encode([new_pk])
+        return unique_string
+
+    def save(self, *args, **kwargs):
+        if not self.short_url:
+            self.short_url = self.get_unique_string
+            self.expiration_date = expiration(days=30)
+        super(URLMapping, self).save(*args, **kwargs)
